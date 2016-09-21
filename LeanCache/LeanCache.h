19500157627471cedb3cf7abe1979ce7dca430d3
Andrew Sowers <asow123@gmail.com>
2016-09-21 11:48:21 -0400

    Initial Commit

---
+++ LeanCache/LeanCache.h
@@ -0,0 +1,19 @@
+//
+//  LeanCache.h
+//  LeanCache
+//
+//  Created by Developer on 9/21/16.
+//  Copyright © 2016 Andrew Sowers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LeanCache.
+FOUNDATION_EXPORT double LeanCacheVersionNumber;
+
+//! Project version string for LeanCache.
+FOUNDATION_EXPORT const unsigned char LeanCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LeanCache/PublicHeader.h>
+
+
