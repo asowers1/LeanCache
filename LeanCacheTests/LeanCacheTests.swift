b933b84b05fd86e55e4e7c11ce0e35206a07cf75
Andrew Sowers <asow123@gmail.com>
2016-09-22 23:21:19 -0400

    updated readme

--- LeanCacheTests/LeanCacheTests.swift
+++ LeanCacheTests/LeanCacheTests.swift
@@ -1,36 +1,13 @@
-//
-//  LeanCacheTests.swift
-//  LeanCacheTests
-//
-//  Created by Developer on 9/21/16.
-//  Copyright © 2016 Andrew Sowers. All rights reserved.
-//
+import Quick
+import Nimble
+import LeanCache
 
-import XCTest
-@testable import LeanCache
-
-class LeanCacheTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+class RGBTests: QuickSpec {
+    override func spec() {
+        describe("LeanCache") {
+            it("works") {
+                expect(true).to(beTrue())
+            }
         }
     }
-    
 }
--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # LeanCache
 [![Version](https://img.shields.io/badge/version-v1.0.0-blue.svg)](https://github.com/KevinVitale/ReactiveJSON/releases/latest)
 ![Platforms](https://img.shields.io/badge/platforms-ios%20%7C%20osx%20%7C%20tvos-lightgrey.svg)
-![Languages](https://img.shields.io/badge/languages-swift-orange.svg)
+![Languages](https://img.shields.io/badge/languages-swift%202.3-orange.svg)
 ![License](https://img.shields.io/badge/license-MIT%2FApache-blue.svg)
 [![Carthage compatible](https://img.shields.io/badge/Carthage-compatible-4BC51D.svg?style=flat)][Carthage]
+[![Cocoapods compatible](https://img.shields.io/badge/Cocoapods-compatable-red.s
+)[Cocoapods]
 
 [Carthage]: https://github.com/carthage/carthage
+[Cocoapods]: https://cocoapods.org
 
 LeanCache allows you to cache and persist plain old objects that conform to NSCoding. To use LeanCache with value based struct models, simply create a class that implements NSCoding and wrap your model.
 
